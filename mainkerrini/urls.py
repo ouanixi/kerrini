cdb470816589d168acf2e6eaa8eaafa941000eb1
Jamie Connelly <jamiebruceconnelly@gmail.com>
2015-11-26 15:31:51 +0000

    Profile pic upload, edit profile, logout working

--- mainkerrini/urls.py
+++ mainkerrini/urls.py
@@ -6,13 +6,13 @@ from mainkerrini import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
-    url(r'^register/login$', views.login, name='login'),
+    url(r'/register/$', views.register, name='register'),
     url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
-    url(r'^picture/$', views.picture, name='picture'),
+    url(r'/login/$', views.login, name='login'),
+    url(r'/logout/$', views.logout, name='logout'),
+    url(r'/picture/$', views.picture, name='picture'),
     url(r'^profile/$', views.profile, name='profile'),
-    url(r'^profile/edit$', views.edit, name='edit'),
-    url(r'^edit/$', views.edit, name='edit'),
+    url(r'/edit$', views.edit, name='edit'),
     url(r'^search/$', views.search, name='search'),
     url(r'^search_result/$', views.search_result, name='search_result'),
 ]
